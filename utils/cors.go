11ce59498167a01c5e6e67f558ab7f2b88e98015
Ajeet Kumar <ajeet8364@gmail.com>
2024-06-21 17:10:29 +0530

    -fixed cors

--- utils/cors.go
+++ utils/cors.go
@@ -22,7 +22,9 @@ func CORSMiddleware() func(c *gin.Context) {
 		}
 
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, origin")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, origin, x-requested-with")
+		// Add AllowCredentials: true
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusNoContent)
